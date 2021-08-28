ab69f256ea0d972837f6171d35637f9571f19d44
Edmilson <scovinowork@gmail.com>
2021-08-28 14:12:02 -0300

    Colocando alguns componentes + navbar

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './view/home/home.component';
+import { NavBarComponent } from './view/nav-bar/nav-bar.component';
+import { FooterComponent } from './view/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +21,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, NavBarComponent, FooterComponent]
 })
 export class AppModule { }
